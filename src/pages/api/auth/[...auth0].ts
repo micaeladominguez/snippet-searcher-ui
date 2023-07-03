d7a020aa2ab2a725d5daa6a7af4407ac66fe73cd
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2023-07-03 14:27:34 -0300

    added token to cookies

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,21 +1,21 @@
-
 // pages/api/auth/[auth0].js
-import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
+import {handleAuth, handleCallback} from '@auth0/nextjs-auth0';
+import Cookies from "js-cookie";
 
 // @ts-ignore
 const afterCallback = (req, res, session, state) => {
-    console.log(session)
-    return session
+  Cookies.set('token', session.idToken, {expires: 7})
+  return session
 };
 
 export default handleAuth({
-    async callback(req, res) {
-        try {
-            // @ts-ignore
-            await handleCallback(req, res, { afterCallback });
-        } catch (error) {
-            // @ts-ignore
-            res.status(error.status || 500).end();
-        }
+  async callback(req, res) {
+    try {
+      // @ts-ignore
+      await handleCallback(req, res, {afterCallback});
+    } catch (error) {
+      // @ts-ignore
+      res.status(error.status || 500).end();
     }
+  }
 });
