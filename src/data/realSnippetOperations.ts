e92d3b71c5f68f6f1a0f101c681e0ef0912e3b6a
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2023-07-04 15:52:45 -0300

    fix URL to push to BE

--- src/data/realSnippetOperations.ts
+++ src/data/realSnippetOperations.ts
@@ -12,18 +12,18 @@ export class RealSnippetOperations implements SnippetOperations {
   }
 
   createSnippet(createSnippet: CreateSnippet): Promise<Snippet> {
-    return axios.post(process.env.NEXT_PUBLIC_API_URL+'/api/snippets', createSnippet)
+    return axios.post(process.env.NEXT_PUBLIC_API_URL+'/backend/snippets', createSnippet)
   }
 
   getSnippetById(id: string): Promise<Snippet | undefined> {
-    return axios.get(process.env.NEXT_PUBLIC_API_URL+'/api/snippets'+id)
+    return axios.get(process.env.NEXT_PUBLIC_API_URL+'/backend/snippets'+id)
   }
 
   listSnippetDescriptors(): Promise<Snippet[]> {
-    return axios.get(process.env.NEXT_PUBLIC_API_URL+'/api/snippets')
+    return axios.get(process.env.NEXT_PUBLIC_API_URL+'/backend/snippets')
   }
 
   updateSnippetById(id: string, updateSnippet: UpdateSnippet): Promise<Snippet> {
-    return axios.patch(process.env.NEXT_PUBLIC_API_URL+'/api/snippets'+id, updateSnippet)
+    return axios.patch(process.env.NEXT_PUBLIC_API_URL+'/backend/snippets'+id, updateSnippet)
   }
 }
