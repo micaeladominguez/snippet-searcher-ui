07b67d7cd49650c52b72e5a6148898d1abbf628e
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2023-07-07 00:24:34 -0300

    fix urls

--- src/data/realSnippetOperations.ts
+++ src/data/realSnippetOperations.ts
@@ -9,23 +9,21 @@ export class RealSnippetOperations implements SnippetOperations {
 
   constructor() {
     autoBind(this)
-    console.log(process.env.NEXT_PUBLIC_API_URL)
   }
 
   createSnippet(createSnippet: CreateSnippet): Promise<Snippet> {
-    return axios.post(process.env.NEXT_PUBLIC_API_URL+'/backend/snippets', createSnippet)
+    return axios.post('/backend/snippets', createSnippet)
   }
 
   getSnippetById(id: string): Promise<Snippet | undefined> {
-    return axios.get(process.env.NEXT_PUBLIC_API_URL+'/backend/snippets/'+id)
+    return axios.get('/backend/snippets/'+id)
   }
 
   listSnippetDescriptors(): Promise<Snippet[]> {
-    console.log(process.env.NEXT_PUBLIC_API_URL+'/backend/snippets/')
-    return axios.get(process.env.NEXT_PUBLIC_API_URL+'/backend/snippets')
+    return axios.get('/backend/snippets')
   }
 
   updateSnippetById(id: string, updateSnippet: UpdateSnippet): Promise<Snippet> {
-    return axios.patch(process.env.NEXT_PUBLIC_API_URL+'/backend/snippets/'+id, updateSnippet)
+    return axios.patch('/backend/snippets/'+id, updateSnippet)
   }
 }
