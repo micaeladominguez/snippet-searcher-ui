8698b26ef9a9e842b6d8a8a9d80a0e7046a48afe
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2023-07-10 13:05:35 -0300

    renaming urls

--- src/data/realSnippetOperations.ts
+++ src/data/realSnippetOperations.ts
@@ -3,27 +3,31 @@ import autoBind from 'auto-bind'
 import axios from "axios";
 import {SnippetOperations} from "@/data/snippetOperations";
 import {Snippet} from "@/util/types";
+import {getCookie} from "cookies-next";
 
 
 export class RealSnippetOperations implements SnippetOperations {
-
+  headers = {
+    'Authorization': `Bearer ${getCookie("token")}`,
+    'Content-Type': 'application/json'
+  }
   constructor() {
     autoBind(this)
   }
 
   createSnippet(createSnippet: CreateSnippet): Promise<Snippet> {
-    return axios.post('/backend/snippets/create', createSnippet)
+    return axios.post('/backend/snippets/create', createSnippet,{headers: this.headers})
   }
 
   getSnippetById(id: string): Promise<Snippet | undefined> {
-    return axios.get('/backend/snippets/'+id)
+    return axios.get('/backend/snippets/'+id,{headers: this.headers})
   }
 
   listSnippetDescriptors(): Promise<Snippet[]> {
-    return axios.get('/backend/snippets')
+    return axios.get('/backend/snippets',{headers: this.headers})
   }
 
   updateSnippetById(id: string, updateSnippet: UpdateSnippet): Promise<Snippet> {
-    return axios.patch('/backend/update/'+id, updateSnippet)
+    return axios.patch('/backend/update/'+id, updateSnippet,{headers: this.headers})
   }
 }
